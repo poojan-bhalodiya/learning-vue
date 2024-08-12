cb416ae14f1f90c3d67ea9a54766a109c77d06fb
Poojan Bhalodiya <poojanbhalodiya02@gmail.com>
2024-08-12 20:55:30 +0530

    Config editor.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-
+import EditoJs from "@editorjs/editorjs"
 </script>
 
 <template>
   <h1 class="text-3xl text-red-500 font-bold underline">
-    Hello World</h1>
+    Your Content</h1>
+  <div id="editorjs"></div>
+  <button>Save articlr</button>
 </template>
 
 <style scoped></style>
