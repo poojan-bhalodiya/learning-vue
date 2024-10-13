347d15acb678701ff2ece504254caf1de3ae82b4
Poojan Bhalodiya <poojanbhalodiya02@gmail.com>
2024-10-13 12:30:16 +0530

    add ref and update cmputed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, computed } from "vue";
+import { reactive, ref, computed } from "vue";
 
 const author = reactive({
   name: "John Doe",
@@ -7,16 +7,42 @@ const author = reactive({
 });
 
 const publishedBooksMessage = computed(() => {
-  return author.books.length > 0 ? "Yes" : "No";
+  if (author.books.length > 0) {
+    return author.books;
+  } else {
+    return "No Boos written";
+  }
+});
+
+// Vue ref
+const firstname = ref("Jay");
+const lastname = ref("Patel");
+
+const fullname = computed({
+  get() {
+    return firstname.value + " " + lastname.value;
+  },
+
+  set(newValue) {
+    [firstname.value, lastname.value] = newValue.split(" ");
+  },
 });
 </script>
 
 <template>
-  <h1 class="text-3xl text-red-500 font-bold underline p-2">Hello World</h1>
-  <p class="p-2 text-rose-600 border border-green-600">
-    Published Books Message
-  </p>
-  <span class="text-sky-700 m-2 p-2">{{ publishedBooksMessage }}</span>
+  <div class="p-2 m-2 border-2 border-sky-900">
+    <p class="p-2 text-rose-600 border border-green-600">
+      Published Books Message
+    </p>
+    <span class="text-sky-700 m-2 p-2">{{ publishedBooksMessage }}</span>
+  </div>
+  <!-- fullname ref -->
+  <div class="p-4 m-6 border-2 border-green-900">
+    <p class="text-blue-600 border border-green-600 p-2">
+      Full Name ref example
+    </p>
+    <span class="p-2 text-pink-700">{{ fullname }}</span>
+  </div>
 </template>
 
 <style scoped></style>
