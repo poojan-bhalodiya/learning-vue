5f3ac08d88fe46ee2a85d2bbc57233bafdcbaf80
poojan-bhalodiya <poojanbhalodiya02@gmail.com>
2024-11-18 08:04:42 +0530

    update ref example

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,16 @@ console.log(refcount);
 function refincript() {
   refcount.value++;
 }
+const obj = ref({
+  nested: { count: 0 },
+  arr: ["foo", "bar"],
+});
+
+function mutateDeeply() {
+  // these will work as expected.
+  obj.value.nested.count++;
+  obj.value.arr.push("baz");
+}
 </script>
 
 <template>
@@ -54,17 +64,30 @@ function refincript() {
       <span class="p-2 text-pink-700">{{ fullname }}</span>
     </div>
   </div>
-  <div>
-    <p class="p-2 m-2 justify-center text-center">Ref Example</p>
-    <div class="border-2 rounded-2xl border-green-500 m-3 p-3">
-      <div class="">{{ refcount }}</div>
-      <div>
-        <button
-          @click="refincript"
-          class="rounded-md border-red-500 border-2 p-1 m-2"
-        >
-          RefCount
-        </button>
+  <div class="p-2">
+    <div class="border-sky-600 border-2 p-2">
+      <p class="m-2 justify-center text-center">Ref Example</p>
+      <div class="border rounded border-green-500 m-3 p-3">
+        <div class="">{{ refcount }}</div>
+        <div>
+          <button
+            @click="refincript"
+            class="rounded border-red-500 border p-1 m-2"
+          >
+            RefCount
+          </button>
+        </div>
+      </div>
+      <div class="p-2 m-2 border border-slate-500">
+        <div>{{ obj }}</div>
+        <div>
+          <button
+            @click="mutateDeeply"
+            class="m-2 p-2 border-red-500 border-2 rounded-md bg-emerald-300"
+          >
+            Deep Reactivity
+          </button>
+        </div>
       </div>
     </div>
   </div>
