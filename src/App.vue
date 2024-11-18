2c3b803de4111ca39c15aa1b5c6be7f1589e50a6
poojan-bhalodiya <poojanbhalodiya02@gmail.com>
2024-11-18 08:03:17 +0530

    add ref example

--- src/App.vue
+++ src/App.vue
@@ -27,22 +27,46 @@ const fullname = computed({
     [firstname.value, lastname.value] = newValue.split(" ");
   },
 });
+
+// Ref Example
+const refcount = ref(0);
+console.log(refcount);
+
+function refincript() {
+  refcount.value++;
+}
 </script>
 
 <template>
-  <div class="p-2 m-2 border-2 border-sky-900">
-    <p class="p-2 text-rose-600 border border-green-600">
-      Published Books Message
-    </p>
-    <span class="text-sky-700 m-2 p-2">{{ publishedBooksMessage }}</span>
+  <div>
+    <p class="p-2 m-2 justify-center text-center">Computed Example</p>
+    <div class="p-2 m-2 border-2 border-sky-900">
+      <p class="p-2 text-rose-600 border border-green-600">
+        Published Books Message
+      </p>
+      <span class="text-sky-700 m-2 p-2">{{ publishedBooksMessage }}</span>
+    </div>
+    <!-- fullname ref -->
+    <div class="p-4 m-6 border-2 border-green-900">
+      <p class="text-blue-600 border border-green-600 p-2">
+        Full Name ref example
+      </p>
+      <span class="p-2 text-pink-700">{{ fullname }}</span>
+    </div>
   </div>
-  <!-- fullname ref -->
-  <div class="p-4 m-6 border-2 border-green-900">
-    <p class="text-blue-600 border border-green-600 p-2">
-      Full Name ref example
-    </p>
-    <span class="p-2 text-pink-700">{{ fullname }}</span>
+  <div>
+    <p class="p-2 m-2 justify-center text-center">Ref Example</p>
+    <div class="border-2 rounded-2xl border-green-500 m-3 p-3">
+      <div class="">{{ refcount }}</div>
+      <div>
+        <button
+          @click="refincript"
+          class="rounded-md border-red-500 border-2 p-1 m-2"
+        >
+          RefCount
+        </button>
+      </div>
+    </div>
   </div>
 </template>
-
 <style scoped></style>
